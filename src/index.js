e67bdaa22a2687151ca4cc02246ccdb006e47850
Vitor Portes <vitorportnoy@gmail.com>
2023-02-03 20:15:29 -0300

    generate img with openai

--- src/index.js
+++ src/index.js
@@ -2,17 +2,29 @@ import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
 
+import photoRoute from "./routes/photoRoute.js";
+import postRoute from "./routes/postRoute.js";
+
+import connectDb from "../mongodb/connect.js";
+
 dotenv.config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
+app.use("/api/post", postRoute);
+app.use("/api/photo", photoRoute);
 
-app.get("/health", (req, res) => {
-  return res.send("OK");
-});
+const startServer = async () => {
+  try {
+    connectDb(process.env.MONGODB_URL);
+    app.listen(8080, () => {
+      console.log("server running");
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-app.listen(4000, () => {
-  console.log("server running");
-});
+startServer();
